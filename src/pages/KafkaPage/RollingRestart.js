df0aa0f1bc3be0c4500cf371ae5f353f001123ea
Petrovich1973 <designtorg@ya.ru>
2020-04-21 00:42:57 +0300

    text content decoration

--- src/pages/KafkaPage/RollingRestart.js
+++ src/pages/KafkaPage/RollingRestart.js
@@ -4,14 +4,18 @@ const RollingRestart = () => {
     return (
         <div style={{width: '36em', maxWidth: '100%'}}>
             <h4>Rolling Restart</h4>
-            <p>The kafka-rolling-restart script can be used to safely restart an entire cluster, one server at a time.
-                The script finds all the servers in a cluster, checks their health status and executes the restart.</p>
+            <div style={{opacity: .6}}>
+                <p>The kafka-rolling-restart script can be used to safely restart an entire cluster, one server at a
+                    time.
+                    The script finds all the servers in a cluster, checks their health status and executes the
+                    restart.</p>
 
-            <h4>Cluster health</h4>
-            <p>The health of the cluster is defined in terms of broker availability and under replicated partitions.
-                Kafka-rolling-restart will check that all brokers are answering to JMX requests, and that the total
-                numer of under replicated partitions is zero. If both conditions are fulfilled, the cluster is
-                considered healthy and the next broker will be restarted.</p>
+                <h4>Cluster health</h4>
+                <p>The health of the cluster is defined in terms of broker availability and under replicated partitions.
+                    Kafka-rolling-restart will check that all brokers are answering to JMX requests, and that the total
+                    numer of under replicated partitions is zero. If both conditions are fulfilled, the cluster is
+                    considered healthy and the next broker will be restarted.</p>
+            </div>
         </div>
     )
 }
