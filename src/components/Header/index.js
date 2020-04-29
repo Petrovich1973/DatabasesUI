46ace33c9534741a94b116c4cfdfe25e5a6182f1
Petrovich1973 <designtorg@ya.ru>
2020-04-29 14:28:32 +0300

    header user added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 import './Header.less'
+import {IconMore, IconUser} from "../../svg";
+import Button from "../Button";
 
 const Header = ({nav = []}) => (
     <header>
+        <div className="left">
+            <span>Web Console</span>
+        </div>
         <nav>
             <ul>
                 {nav
@@ -20,40 +25,12 @@ const Header = ({nav = []}) => (
                     })}
             </ul>
         </nav>
+        <div className="right">
+            <IconUser size={'1.6em'}/>
+            <span className="user-name"><small>Administrator Ivanov Ivan Ivanovich</small></span>
+            <Button icon={<IconMore size={'1em'}/>} className="sm"/>
+        </div>
     </header>
 )
-
-// const Header = ({nav = []}) => (
-//     <header>
-//         <nav>
-//             <ul>
-//                 {nav
-//                     .map(route => {
-//                         if (route.headerNav && route.childs && route.childs.length) {
-//                             return (
-//                                 <li key={route.path}>
-//                                     <ul>
-//                                         {route.childs.map((subRoute, idxSub) => (
-//                                             <li key={idxSub}>
-//                                                 <NavLink to={route.path + subRoute.path}>
-//                                                     <span>{subRoute.title}</span>
-//                                                 </NavLink>
-//                                             </li>
-//                                         ))}
-//                                     </ul>
-//                                 </li>
-//                             )
-//                         } else {
-//                             return (
-//                                 <li key={route.path}>
-//                                     <NavLink to={route.path}>{route.title}</NavLink>
-//                                 </li>
-//                             )
-//                         }
-//                     })}
-//             </ul>
-//         </nav>
-//     </header>
-// )
 
 export default Header
--- src/svg/index.js
+++ src/svg/index.js
@@ -6,6 +6,9 @@ export { IconOverview } from './IconOverview'
 export { IconBroker } from './IconBroker'
 export { IconConsumers } from './IconConsumers'
 
+export { IconUser } from './IconUser'
+export { IconMore } from './IconMore'
+
 export { IconDelete } from './IconDelete'
 export { IconFile } from './IconFile'
 export { IconFolder } from './IconFolder'
